c391bf017b854d14ea21c60b39555204434a03e4
OlegSam2 <doorsmania@mail.ru>
2022-08-17 19:05:10 +0300

    homework array 8 elements

---
+++ task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+//  и выводит их на экран. Вводим массив через консоль
+
+int[] array = new int [8];	
+	
+for (int i = 0; i < array.Length; i++)	
+{	
+ int n = 1+i;	
+ Console.Write("\n Введите " + n + " элемент массива : " );	
+ array[i] = int.Parse(Console.ReadLine());	
+}	
+	
+Console.WriteLine("\n Введен массив из 8 элементов");	
+	
+Console.WriteLine(string.Join(" ; ", array));	
+
+
